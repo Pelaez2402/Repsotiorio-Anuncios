6a8f60ca6e43a7678efab81817963556443ee8a3
Tomas Vargas <tomasvargasmuneton2007@gmail.com>
2025-11-05 21:22:58 -0500

    interfaces faltante

---
+++ ANUNCIOS/lib_presentaciones/Implementaciones/CategoriasPresentacion.cs
@@ -0,0 +1,115 @@
+﻿using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Interfaces;
+using System.Diagnostics.Metrics;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class CategoriasPresentacion : IAnunciosPresentacion
+    {
+        private Comunicaciones? comunicaciones = null;
+
+        public async Task<List<Anuncios>> Listar()
+        {
+            var lista = new List<Anuncios>();
+            var datos = new Dictionary<string, object>();
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Anuncios/Listar");
+            var respuesta = await comunicaciones!.Ejecutar(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Anuncios>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<List<Anuncios>> PorTitulo(Anuncios? entidad)
+        {
+            var lista = new List<Anuncios>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad!;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Anuncios/PorTipo");
+            var respuesta = await comunicaciones!.Ejecutar(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Anuncios>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<Anuncios?> Guardar(Anuncios? entidad)
+        {
+            if (entidad!.Id != 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Anuncios/Guardar");
+            var respuesta = await comunicaciones!.Ejecutar(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Anuncios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Anuncios?> Modificar(Anuncios? entidad)
+        {
+            if (entidad!.Id == 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Anuncios/Modificar");
+
+            var respuesta = await comunicaciones!.Ejecutar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Anuncios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Anuncios?> Borrar(Anuncios? entidad)
+        {
+            if (entidad!.Id == 0)
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            comunicaciones = new Comunicaciones();
+            datos = comunicaciones.ConstruirUrl(datos, "Anuncios/Borrar");
+            var respuesta = await comunicaciones!.Ejecutar(datos);
+
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Anuncios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
---
+++ ANUNCIOS/lib_presentaciones/Interfaces/IAnunciosSubcategoriasPresentacion.cs
@@ -0,0 +1,14 @@
+﻿using lib_dominio.Entidades;
+using System.Diagnostics.Metrics;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface IAnunciosSubcategoriasPresentacion
+    {
+        Task<List<Anuncios>> Listar();
+        Task<List<Anuncios>> PorTitulo(Anuncios? entidad);
+        Task<Anuncios?> Guardar(Anuncios? entidad);
+        Task<Anuncios?> Modificar(Anuncios? entidad);
+        Task<Anuncios?> Borrar(Anuncios? entidad);
+    }
+}
